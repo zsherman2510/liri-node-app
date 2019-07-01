f0b05ec6a726cc91c3e24a5b32c9c21a40332bbe
zavion sherman <zsherman2510@hotmail.com>
2019-06-30 23:27:25 -0700

    finished the spotify function

--- liri.js
+++ liri.js
@@ -1,10 +1,42 @@
+console.log("js running");
+
 require("dotenv").config();
+
+console.log("back here");
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
-var spotify = new spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
+
+var getArtistNames = function(artist) {
+  return artist.name;
+};
+var fetchSpotify = function(songName) {
+  spotify.search(
+    {
+      type: "track",
+      query: songName
+    },
+    function(err, data) {
+      if (err) {
+        console.log("Error occurred:" + err);
+        return;
+      }
+
+      var songs = data.tracks.items;
+      songs.forEach(function(song, i) {
+        console.log(i);
+        console.log("artist(s):" + song.artists.map(getArtistNames));
+        console.log("song name:" + song.name);
+        console.log("preview song:" + song.preview_url);
+        console.log("album:" + song.album.name);
+        console.log("----------------------------");
+      });
+    }
+  );
+};
 
 var commands = function(caseData, functionData) {
   switch (caseData) {
@@ -28,3 +60,10 @@ var commands = function(caseData, functionData) {
       console.log("Liri doesnt know that");
   }
 };
+
+var commandThis = function(argOne, argTwo) {
+  commands(argOne, argTwo);
+  console.log(process.argv);
+};
+
+commandThis(process.argv[2], process.argv.slice(3).join(" "));
